a34c986ffa6294478a2ec01c956948ec786441da
Elias Slettemark <eliasslettemark@gmail.com>
2025-08-24 15:17:07 +0200

    commit

--- app/auth/AuthClient.tsx
+++ app/auth/AuthClient.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { Button } from "frosted-ui";
+import { Button, Theme } from "frosted-ui";
 import localFont from "next/font/local";
 import WhopLogo from "./WhopLogo";
 import { signIn } from "next-auth/react";
@@ -14,15 +14,17 @@ const AuthClient = () => {
   return (
     <html lang="en" suppressHydrationWarning className={interVariable.variable}>
       <body>
-        <div className="w-full h-screen flex items-center justify-center bg-gray-1">
-          <Button
-            variant="classic"
-            size="4"
-            onClick={() => signIn("whop")}
-          >
-            Sign in with <WhopLogo className="w-[137px] h-auto" />
-          </Button>
-        </div>
+        <Theme asChild appearance="dark" grayColor="gray" accentColor="blue">
+          <div id="root" className="w-full h-screen bg-gray-1 flex items-center justify-center">
+            <Button
+              variant="classic"
+              size="4"
+              onClick={() => signIn("whop")}
+            >
+              Sign in with <WhopLogo className="w-[137px] h-auto" />
+            </Button>
+          </div>
+        </Theme>
       </body>
     </html>
   );
